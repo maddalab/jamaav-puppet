4ac2020bf6298d84943ad714186e2b31e8ca7e17
Bhaskar Maddala <bhaskar@tumblr.com>
2014-03-01 19:40:12 -0500

    fix issue in node terminus

--- node_terminus.rb
+++ node_terminus.rb
@@ -4,28 +4,36 @@ require "yaml"
 host_name = Socket::gethostname
 match_data = host_name.match('(.*)-\d\.(.*)\.jamaav.net')
 if (match_data.nil?)
-	raise "Malformed hostname, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+	raise "Malformed hostname #{host_name}, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
+
 nodetype = match_data[1]
 if (nodetype.nil?)
-	raise "Malformed hostname, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
+
 environment = match_data[2]
 if (environment.nil?)
-	raise "Malformed hostname, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
 
 # load custom parameters
 if !File.exists?("/var/db/config.yml")
-	raise "Configuration file does not exists. It must include the basic information about the node, ex:name and can provide additional information for package version"
+	raise "Configuration file /var/db/config.yml does not exists.
+       	It must include the basic information about the node, ex:name and can provide additional information for package version"
+end
 
 config_data = YAML.load_file("/var/db/config.yml")
 
 # write out the yaml for node terminus
-print "---"
-print "environment:"
-print "  %s:" % environment
-print "classes:"
-print "  %snode:" % nodetype
-print "parameters:"
+puts "---"
+puts "environment:"
+puts "  #{environment}"
+puts "classes:"
+puts "  %snode:" % nodetype
+puts "  #{nodetype}node:"
+puts "parameters:"
 config_data.each do |key, value|
-	print "  %s: \"%s\"" % (key, value)
+	puts "  #{key}: #{value}"
 end
 
