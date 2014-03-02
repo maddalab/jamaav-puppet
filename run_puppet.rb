2999e31b21071854c6c6e55dc351c09c81d7e412
Bhaskar Maddala <bhaskar@tumblr.com>
2014-03-02 18:24:45 -0500

    Make running of puppet easier taking into account the environment

---
+++ run_puppet.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require "socket"
+
+host_name = Socket::gethostname
+match_data = host_name.match('(.*)-\d\.(.*)\.jamaav.net')
+if (match_data.nil?)
+	raise "Malformed hostname #{host_name}, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
+
+nodetype = match_data[1]
+if (nodetype.nil?)
+	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
+config_data["nodeclass"] = nodetype
+
+environment = match_data[2]
+if (environment.nil?)
+	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
+end
+
+res = system("/usr/bin/puppet", "apply", "/etc/puppet/environments/#{environment}/manifests/site.pp", "--environment=#{environment}")
