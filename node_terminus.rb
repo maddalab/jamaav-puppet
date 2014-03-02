500b8a3fbcb5e20fc69ba8b84f74a27fbed4e361
Bhaskar Maddala <bhaskar@tumblr.com>
2014-03-01 19:47:35 -0500

    improvement to nodeclass

--- node_terminus.rb
+++ node_terminus.rb
@@ -3,6 +3,7 @@ require "yaml"
 
 host_name = Socket::gethostname
 match_data = host_name.match('(.*)-\d\.(.*)\.jamaav.net')
+config_data = {}
 if (match_data.nil?)
 	raise "Malformed hostname #{host_name}, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
 end
@@ -11,11 +12,13 @@ nodetype = match_data[1]
 if (nodetype.nil?)
 	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
 end
+config_data["nodeclass"] = nodetype
 
 environment = match_data[2]
 if (environment.nil?)
 	raise "Malformed hostname #{host_name}, cannot determine nodetype, hostname of the format <nodetype>-<index>.<datacenter>.jamaav.net is expected"
 end
+config_data["environment"] = environment
 
 # load custom parameters
 if !File.exists?("/var/db/config.yml")
@@ -23,15 +26,14 @@ if !File.exists?("/var/db/config.yml")
        	It must include the basic information about the node, ex:name and can provide additional information for package version"
 end
 
-config_data = YAML.load_file("/var/db/config.yml")
+config_data.merge(YAML.load_file("/var/db/config.yml").to_hash)
 
 # write out the yaml for node terminus
 puts "---"
 puts "environment:"
-puts "  #{environment}"
+puts "  #{config_data["environment"]}"
 puts "classes:"
-puts "  %snode:" % nodetype
-puts "  #{nodetype}node:"
+puts "  #{config_data["nodeclass"]}node:"
 puts "parameters:"
 config_data.each do |key, value|
 	puts "  #{key}: #{value}"
