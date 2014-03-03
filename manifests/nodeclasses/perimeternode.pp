91b298439af45a2de19b24d8643dd796f5983b7a
Bhaskar Maddala <bhaskar@tumblr.com>
2014-03-02 19:09:26 -0500

    install haproxy on the perimeter nodes

--- manifests/nodeclasses/perimeternode.pp
+++ manifests/nodeclasses/perimeternode.pp
@@ -1,3 +1,8 @@
 class perimeternode {
     notify { "Starting puppet execution for Jamaav perimeter nodes": }
+
+    # install haproxy on the perimeter nodes
+    package { 'haproxy':
+        ensure => '1.4.18-0ubuntu1.2'
+    }
 }
